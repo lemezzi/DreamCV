b0b9dab3845df86a5731caac2fc0c4ea5cdecd80
lemezzi <101637678+lemezzi@users.noreply.github.com>
2024-05-19 18:59:08 +0100

    Update store.js

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -2,8 +2,17 @@ import { configureStore } from "@reduxjs/toolkit";
 import CvSlice from "../features/CvSlice";
 import EducationSlice from "../features/EducationSlice";
 import ProfSlice from "../features/ProfSlice";
+import CompetSlice from "../features/CompetSlice"; // Importez le CompetSlice
+import LanguageSlice from "../features/LanguageSlice"; // Importez le CompetSlice
 
 
-export default configureStore({reducer:{personal:CvSlice,education:EducationSlice,prof:ProfSlice,
-                                        
-}})
+export default configureStore({
+  reducer: {
+    personal: CvSlice,
+    education: EducationSlice,
+    prof: ProfSlice,
+    compet: CompetSlice, 
+    long:LanguageSlice,
+
+  },
+});
