2fa6241f0c98f560383d1a43054f56747d53e7e9
lemezzi <101637678+lemezzi@users.noreply.github.com>
2024-05-20 12:38:15 +0100

    Update Personal.js

--- src/Components/Personal.js
+++ src/Components/Personal.js
@@ -191,7 +191,7 @@ function Personal() {
           <div className='descrip'> <p>Desctiprion:{personal.description}</p> </div>
           
           <button onClick={() => handleEdit(index)}>Modifier</button>
-          <button onClick={() => handleDelete(index)}>Supprimer</button>
+          <button className="delete" onClick={() => handleDelete(index)}>Supprimer</button>
         </div>
       ))}
     </div>
