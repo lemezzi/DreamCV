7b615a1fc3f8f3241112fbe80299738b98861aa2
lemezzi <101637678+lemezzi@users.noreply.github.com>
2024-05-24 23:52:52 +0100

    Update CvSlice.js

--- src/features/CvSlice.js
+++ src/features/CvSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 
 export const cvSlice = createSlice({
   name: "personal",
-  initialState: { nom: "", teleph: "", mail: "",adress:"" ,description:""},
+  initialState: { nom: "", teleph: "", mail: "", adress: "", description: "", image: null },
   reducers: {
     changeName: (state, action) => {
       if (action.payload && action.payload.nom) {
@@ -26,28 +26,35 @@ export const cvSlice = createSlice({
     },
     changeGithub: (state, action) => {
       if (action.payload && action.payload.github) {
-        state.github = action.payload.github;
-      }
-    },
-    changeLinkedin: (state, action) => {
-      if (action.payload && action.payload.linkedin) {
-        state.linkedin = action.payload.linkedin;
-      }
-    },
-    changeDescrip: (state, action) => {
-      if (action.payload && action.payload.description) {
-        state.description = action.payload.description;
+        state.github = action.payload.github}
+      },
+      changeLinkedin: (state, action) => {
+        if (action.payload && action.payload.linkedin) {
+          state.linkedin = action.payload.linkedin;
+        }
+      },
+      changeDescrip: (state, action) => {
+        if (action.payload && action.payload.description) {
+          state.description = action.payload.description;
+        }
+      },
+      changeImage: (state, action) => {
+        if (action.payload && action.payload.imageUrl) {
+          state.image = action.payload.imageUrl;
+        }
       }
     }
-  }
-});
-
-export const { changeName, changeTelph, changeMail,changeAdress,changeGithub,changeLinkedin ,changeDescrip} = cvSlice.actions; // Ajoutez changeMail ici
-export const getNom = (state) => state.personal.nom;
-export const getTelph = (state) => state.personal.teleph;
-export const getMail = (state) => state.personal.mail;
-export const getAdress=(state)=>state.personal.adress;
-export const getGit=(state)=>state.personal.github;
-export const getLink=(state)=>state.personal.linkedin;
-export const getDescrip=(state)=>state.personal.description;
-export default cvSlice.reducer;
+   });
+   
+   export const { changeImage, changeName, changeTelph, changeMail, changeAdress, changeGithub, changeLinkedin, changeDescrip } = cvSlice.actions;
+   
+   export const getNom = (state) => state.personal.nom;
+   export const getTelph = (state) => state.personal.teleph;
+   export const getMail = (state) => state.personal.mail;
+   export const getAdress = (state) => state.personal.adress;
+   export const getGit = (state) => state.personal.github;
+   export const getLink = (state) => state.personal.linkedin;
+   export const getDescrip = (state) => state.personal.description;
+   export const getImage = (state) => state.personal.image;
+   
+   export default cvSlice.reducer;
