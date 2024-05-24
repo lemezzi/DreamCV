20e9db17483e6db6bde0c1e2c55ad80fc7b5b659
lemezzi <101637678+lemezzi@users.noreply.github.com>
2024-05-24 23:52:27 +0100

    Update Langue.js

--- src/Components/Langue.js
+++ src/Components/Langue.js
@@ -79,6 +79,7 @@ function Langue() {
               <option value="B2">B2</option>
               <option value="C1">C1</option>
               <option value="C2">C2</option>
+              <option value="Maternelle">Maternelle</option>
             </select>
           </div>
           <button type="submit">{editingIndex !== null ? "Modifier" : <>Ajouter <FaPlus /></>}</button>
